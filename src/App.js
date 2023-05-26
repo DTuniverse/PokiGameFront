f88df1694336307f0b7a77c0ee6fbd6c1e4deee5
Quandt <danielquandt@web.de>
2023-05-26 17:29:44 +0200

    fightercomponentlogic

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Arena from "./components/Arena";
 import LandingPage from "./components/LandingPage";
 import Modal from "./components/Modal";
 import data from "./data.json";
+import Fighter from "./components/Fighter";
 console.log(data);
 
 function App() {
   return (
     <div className="App">
+      <Fighter />
       <Routes>
         <Route path="/" element={<LandingPage data={data} />} />
         <Route path="/pokemon/:id" element={<Modal />} />
