30f97bba7816ab9eb5c1624eff408127e17a59d3
DTuniverse <127115560+DTuniverse@users.noreply.github.com>
2023-05-29 17:29:36 +0200

    Added global heading. Added recources folder. PokiCardOnLanding component. pokiCardOnLandingCss.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,3 +1,16 @@
+import PokiCardOnLanding from "./PokiCardOnLanding";
+import "./pokiCardOnLanding.css";
+
 export default function LandingPage({ data }) {
-  return <div>{console.log("Landingpage1", data[0])}</div>;
+  return (
+    <div>
+      <h1 id="global-heading">PokiFIGHT</h1>
+      <div className="parent-pokiCard">
+        {data?.allPokemon &&
+          data.allPokemon.map((poki, index) => (
+            <PokiCardOnLanding poki={poki} index={index} />
+          ))}
+      </div>
+    </div>
+  );
 }
