d9144d7ae60f38c89e43e83102599c5182636653
Anton <avonpoblozki@posteo.de>
2023-06-06 13:27:52 +0200

    styling modal and bg

--- src/components/Modal_Card.jsx
+++ src/components/Modal_Card.jsx
@@ -10,6 +10,7 @@ import {Link} from "react-router-dom"
 import TextField from '@mui/material/TextField';
 import { CardHeader } from '@mui/material';
 
+
 export default function BasicCard({data, dataImg, indexOfSelected, modalImg, setSelectedPoke, handleClose, playerName, setPlayerName}) {
   const pokemon = data[indexOfSelected]
   const handleClick=()=>{
@@ -23,11 +24,11 @@ export default function BasicCard({data, dataImg, indexOfSelected, modalImg, set
   
   return (
     <Card sx={{ minWidth: 300, maxWidth:400, color:"white"}}>
-      
-      <CardContent>
+      {/* font-family: Shojumaru, cursive; */}
+      <CardContent >
       
         
-          <Typography sx={{textAlign:"center"}} variant="h4">{data[indexOfSelected].name.english}</Typography>
+          <Typography sx={{textAlign:"center", fontFamily:"Shojumaru, cursive"}} variant="h4">{data[indexOfSelected].name.english}</Typography>
         
         <Box sx={{display:"flex",flexDirection:"row", alignItems:'center', justifyContent:'center'}}>
         
@@ -38,9 +39,9 @@ export default function BasicCard({data, dataImg, indexOfSelected, modalImg, set
                 title={`Image of ${data[indexOfSelected]?.name.english}`}
             />
             <Box sx={{flexGrow: 1, textAlign:'center'}}>
-                <Typography>HP: {data[indexOfSelected]?.base.HP}</Typography> {/* <Box component='span' sx={{width:4,backgroundColor:'green'}} /> */}
-                <Typography>Attack: {data[indexOfSelected]?.base.Attack}</Typography>
-                <Typography>Defense: {data[indexOfSelected]?.base.Defense}</Typography>
+                <Typography sx={{fontFamily:"Shojumaru, cursive"}}>HP: {data[indexOfSelected]?.base.HP}</Typography> {/* <Box component='span' sx={{width:4,backgroundColor:'green'}} /> */}
+                <Typography sx={{fontFamily:"Shojumaru, cursive"}}>Attack: {data[indexOfSelected]?.base.Attack}</Typography>
+                <Typography sx={{fontFamily:"Shojumaru, cursive"}}>Defense: {data[indexOfSelected]?.base.Defense}</Typography>
             </Box>
         </Box> 
         
@@ -53,10 +54,10 @@ export default function BasicCard({data, dataImg, indexOfSelected, modalImg, set
          */}
       </CardContent>
       <CardActions sx={{flexDirection:"column"}}>
-        <input type="text" className="playerName" value={playerName} placeholder="Enter your Name here" onChange={e=>setPlayerName(e.target.value)}  />
+        <input style={{fontFamily:"Shojumaru, cursive"}} type="text" className="playerName" value={playerName} placeholder="Enter your Name here" onChange={e=>setPlayerName(e.target.value)}  />
         <br />
-        <Button onClick={handleClick} size="medium" fullWidth="true" sx={{ color:"white", ":hover": {bgcolor: "green",color: "white"}}}><Link id="btn_choose" to="/pokemon/arena">I choose you {data[indexOfSelected]?.name.english}</Link></Button>
-        <Button onClick={handleClose} size="medium" fullWidth="true" sx={{ color:"white", ":hover": {bgcolor: "red",color: "white"}}}>Not this Time!</Button>
+        <Button onClick={handleClick} size="medium" fullWidth="true" sx={{ color:"white", ":hover": {bgcolor: "green",color: "white"}, fontFamily:"Shojumaru, cursive"}}><Link id="btn_choose" to="/pokemon/arena">I choose you {data[indexOfSelected]?.name.english}</Link></Button>
+        <Button onClick={handleClose} size="medium" fullWidth="true" sx={{ color:"white", ":hover": {bgcolor: "red",color: "white"},fontFamily:"Shojumaru, cursive"}}>Not this Time!</Button>
       </CardActions>
     </Card>
   );
